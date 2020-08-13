e5a420acb391929157b70503e255958b617e6b37
patrickgdl <patrickgdlima@gmail.com>
2020-08-13 14:55:50 -0300

    feat(layout): layout grid tests

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ju-sidebar',
+  template: `
+    <p>
+      sidebar works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
