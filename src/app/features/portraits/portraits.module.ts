38a8f178426afff3fea7d3cbc23d9b9339576079
patrickgdl <patrickgdlima@gmail.com>
2020-10-14 00:14:03 -0300

    feat(logo): componentization

--- src/app/features/portraits/portraits.module.ts
+++ src/app/features/portraits/portraits.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { DialogModule } from '../../components/dialog/dialog.module';
+import { LogoModule } from './../../components/logo/logo.module';
 import { PortraitsRoutingModule } from './portraits-routing.module';
 import { PortraitsComponent } from './portraits.component';
 
 @NgModule({
   declarations: [PortraitsComponent],
-  imports: [CommonModule, PortraitsRoutingModule, DialogModule],
+  imports: [CommonModule, PortraitsRoutingModule, DialogModule, LogoModule],
 })
 export class PortraitsModule {}
